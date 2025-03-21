805fe0aae6ef0ae3ce647adebfdefebb8668e8eb
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2025-03-21 11:58:58 +0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,26 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-    }
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+  base: '/dist/',  // Ensure the assets are loaded from the correct directory
 });
